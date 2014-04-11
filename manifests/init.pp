a07e21b19e7b20bb0c47df9be0b704487dad5332
Joel Friedlaender <joel@redguava.com.au>
2014-04-11 16:34:48 +1000

    Update to version 4.2.2 of 1Password.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #     include 1password
 class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
-  $path = 'dist/1P/mac4/1Password-4.0.9.zip'
+  $path = 'dist/1P/mac4/1Password-4.2.2.zip'
 
   package { '1Password':
     source   => "https://${host}/${path}",
